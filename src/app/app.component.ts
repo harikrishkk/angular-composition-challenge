3bd307dd48806db60afb636c205e9ef01bd2c5ef
Hari <164977095+thecanadiandev@users.noreply.github.com>
2025-03-03 14:28:18 -0500

    commit 2: hello world commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+   <h1> Hello Angular </h1>
+  `,
 })
 export class AppComponent {
-  title = 'angular-demo-application';
+
 }
